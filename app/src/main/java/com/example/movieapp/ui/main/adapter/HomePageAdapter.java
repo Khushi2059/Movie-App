f85106f66ed2c9c184f378fe34a1211c4826f8a3
Khushi2059 <skhushi2083@gmail.com>
2022-01-20 22:54:40 +0530

    Changed

--- app/src/main/java/com/example/movieapp/ui/main/adapter/HomePageAdapter.java
+++ app/src/main/java/com/example/movieapp/ui/main/adapter/HomePageAdapter.java
@@ -13,27 +13,27 @@ import androidx.viewpager.widget.PagerAdapter;
 import com.bumptech.glide.Glide;
 import com.example.movieapp.R;
 import com.example.movieapp.model.BannerMovies;
-import com.example.movieapp.ui.main.view.MovieDetails;
+import com.example.movieapp.ui.main.view.MovieDetailsActivity;
 
 import java.util.List;
 
-public class BannerMoviesPagesAdapter4 extends PagerAdapter {
+public class HomePageAdapter extends PagerAdapter {
     Context context;
-    List<BannerMovies> tvShowBannerList;
+    List<BannerMovies> homeBannerList;
 
-    public BannerMoviesPagesAdapter4(Context context, List<BannerMovies> bannerMoviesList) {
+    public HomePageAdapter(Context context, List<BannerMovies> homeBannerList) {
         this.context= context;
-        this.tvShowBannerList = bannerMoviesList;
+        this.homeBannerList = homeBannerList;
     }
 
-    public void setBannerMoviesList4(List<BannerMovies> bannerMoviesList) {
-        this.tvShowBannerList = bannerMoviesList;
+    public void setHomePageAdapter(List<BannerMovies> homeBannerList) {
+        this.homeBannerList = homeBannerList;
         notifyDataSetChanged();
     }
 
     @Override
     public int getCount() {
-        return tvShowBannerList.size();
+        return homeBannerList.size();
     }
 
     @Override
@@ -51,17 +51,17 @@ public class BannerMoviesPagesAdapter4 extends PagerAdapter {
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
         View view= LayoutInflater.from(context).inflate(R.layout.banner_movie_layout,null);
         final ImageView bannerImage=view.findViewById(R.id.banner_image);
-        Glide.with(context).load("https://image.tmdb.org/t/p/original" + tvShowBannerList.get(position).getPoster_path()).into(bannerImage);
+        Glide.with(context).load("https://image.tmdb.org/t/p/original" + homeBannerList.get(position).getPoster_path()).into(bannerImage);
         container.addView(view);
 
         bannerImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(context, MovieDetails.class);
-                i.putExtra("id", tvShowBannerList.get(position).getId());
-                i.putExtra("original_name", tvShowBannerList.get(position).getOriginal_title());
-                i.putExtra("poster_path","https://image.tmdb.org/t/p/original"+ tvShowBannerList.get(position).getPoster_path());
-                i.putExtra("movieFile", tvShowBannerList.get(position).getVideo());
+                Intent i=new Intent(context, MovieDetailsActivity.class);
+                i.putExtra("id",homeBannerList.get(position).getId());
+                i.putExtra("original_name",homeBannerList.get(position).getOriginal_title());
+                i.putExtra("poster_path","https://image.tmdb.org/t/p/original"+homeBannerList.get(position).getPoster_path());
+                i.putExtra("movieFile",homeBannerList.get(position).getVideo());
                 context.startActivity(i);
             }
         });
--- app/src/main/java/com/example/movieapp/ui/main/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/com/example/movieapp/ui/main/viewmodel/MainActivityViewModel.java
@@ -1,53 +1,44 @@
 package com.example.movieapp.ui.main.viewmodel;
 
-import android.os.AsyncTask;
+import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
 
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
-import com.example.movieapp.ui.main.adapter.Adapter;
-import com.example.movieapp.ui.main.adapter.BannerMoviesPagesAdapter;
-import com.example.movieapp.ui.main.adapter.BannerMoviesPagesAdapter2;
-import com.example.movieapp.ui.main.adapter.BannerMoviesPagesAdapter3;
-import com.example.movieapp.ui.main.adapter.BannerMoviesPagesAdapter4;
+import com.example.movieapp.model.MainRecyclerRepo;
+import com.example.movieapp.model.MovieModelClass;
+import com.example.movieapp.ui.main.adapter.RecyclerViewAdapter;
+import com.example.movieapp.ui.main.adapter.KidsPageAdapter;
+import com.example.movieapp.ui.main.adapter.HomePageAdapter;
+import com.example.movieapp.ui.main.adapter.MoviesPageAdapter;
+import com.example.movieapp.ui.main.adapter.TvPageAdapter;
 import com.example.movieapp.model.BannerMovies;
 import com.example.movieapp.model.HomeResp;
 import com.example.movieapp.model.KidsResp;
-import com.example.movieapp.model.MovieModelClass;
 import com.example.movieapp.model.MovieResp;
 import com.example.movieapp.model.TvResp;
 import com.example.movieapp.retrofit.RetrofitClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.TimerTask;
+
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivityViewModel {
-    private static String BASE_URL="https://api.themoviedb.org/3/tv/airing_today?api_key=232be951f20b306084f1a1bcc66a1081";
 
-
-    public void getBannerData(BannerMoviesPagesAdapter bannerMoviesPagesAdapter){
+    public void getBannerData(HomePageAdapter homePageAdapter){
         CompositeDisposable compositeDisposable=new CompositeDisposable();
-        boolean bannerData = compositeDisposable.add(RetrofitClient.getRetrofitClient().getMovieResp()
+        compositeDisposable.add(RetrofitClient.getRetrofitClient().getMovieResp()
                 .subscribeOn(Schedulers.io())
                 .subscribeWith(new DisposableObserver<MovieResp>() {
                     @Override
                     public void onNext(MovieResp moviesResp) {
                         //   Log.d("bannerData", moviesResp.getMoviesList().toString());
                         List<BannerMovies> movieBannerList = moviesResp.getMoviesList();
-                        bannerMoviesPagesAdapter.setKidsBannerList(movieBannerList);
+                        homePageAdapter.setHomePageAdapter(movieBannerList);
                     }
 
                     @Override
@@ -63,16 +54,16 @@ public class MainActivityViewModel {
 
         );
     }
-    public void getBannerData1(BannerMoviesPagesAdapter2 bannerMoviesPagesAdapter2){
+    public void getBannerData1(TvPageAdapter tvPageAdapter){
         CompositeDisposable compositeDisposable=new CompositeDisposable();
-        boolean bannerData1 = compositeDisposable.add(RetrofitClient.getRetrofitClient().getHomeResp()
+        compositeDisposable.add(RetrofitClient.getRetrofitClient().getHomeResp()
                 .subscribeOn(Schedulers.io())
                 .subscribeWith(new DisposableObserver<HomeResp>() {
                     @Override
                     public void onNext(HomeResp homeResp) {
                         //  Log.d("bannerData", homeResp.getMoviesList().toString());
                         List<BannerMovies> homeBannerList= homeResp.getMoviesList();
-                        bannerMoviesPagesAdapter2.setBannerMoviesList2(homeBannerList);
+                        tvPageAdapter.setTvPageAdapter(homeBannerList);
                     }
 
                     @Override
@@ -88,16 +79,16 @@ public class MainActivityViewModel {
 
         );
     }
-    public void getBannerData2(BannerMoviesPagesAdapter3 bannerMoviesPagesAdapter3){
+    public void getBannerData2(MoviesPageAdapter moviesPageAdapter){
         CompositeDisposable compositeDisposable=new CompositeDisposable();
-        boolean bannerData2 = compositeDisposable.add(RetrofitClient.getRetrofitClient().getTvResp()
+        compositeDisposable.add(RetrofitClient.getRetrofitClient().getTvResp()
                 .subscribeOn(Schedulers.io())
                 .subscribeWith(new DisposableObserver<TvResp>() {
                     @Override
                     public void onNext(TvResp tvResp) {
                         //  Log.d("bannerData", tvResp.getMoviesList().toString());
                         List<BannerMovies> tvShowBannerList = tvResp.getMoviesList();
-                        bannerMoviesPagesAdapter3.setBannerMoviesList3(tvShowBannerList);
+                        moviesPageAdapter.setBannerMoviesList3(tvShowBannerList);
                     }
 
                     @Override
@@ -115,16 +106,16 @@ public class MainActivityViewModel {
     }
 
 
-    public void getBannerData3(BannerMoviesPagesAdapter4 bannerMoviesPagesAdapter4){
+    public void getBannerData3(KidsPageAdapter kidsPageAdapter){
         CompositeDisposable compositeDisposable=new CompositeDisposable();
-        boolean bannerData3 = compositeDisposable.add(RetrofitClient.getRetrofitClient().getWatchNext()
+        compositeDisposable.add(RetrofitClient.getRetrofitClient().getWatchNext()
                 .subscribeOn(Schedulers.io())
                 .subscribeWith(new DisposableObserver<KidsResp>() {
                     @Override
                     public void onNext(KidsResp kidsResp) {
                         //  Log.d("bannerData", tvResp.getMoviesList().toString());
                         List<BannerMovies> kidsBannerList = kidsResp.getMoviesList();
-                        bannerMoviesPagesAdapter4.setBannerMoviesList4(kidsBannerList);
+                        kidsPageAdapter.setKidsPageAdapter(kidsBannerList);
                     }
 
                     @Override
@@ -142,89 +133,33 @@ public class MainActivityViewModel {
     }
 
 
+    public void getBannerData4(RecyclerViewAdapter recyclerViewAdapter) {
 
+        CompositeDisposable compositeDisposable=new CompositeDisposable();
+        compositeDisposable.add(RetrofitClient.getRetrofitClient().geImgData()
+                .subscribeOn(Schedulers.io())
+                .subscribeWith(new DisposableObserver<MainRecyclerRepo>() {
 
-
-    public static class GetData extends AsyncTask<String,String,String>
-    {
-        Adapter adapter;
-        RecyclerView recyclerView;
-        LinearLayoutManager linearLayoutManager;
-        public GetData(Adapter adapter,RecyclerView recyclerView,LinearLayoutManager linearLayoutManager) {
-            this.adapter=adapter;
-            this.recyclerView=recyclerView;
-            this.linearLayoutManager=linearLayoutManager;
-        }
-
-        @Override
-        protected String doInBackground(String... strings) {
-            String current="";
-            try{
-                URL url;
-                HttpURLConnection urlConnection=null;
-                try {
-                    url=new URL(BASE_URL);
-                    urlConnection=(HttpURLConnection) url.openConnection();
-
-                    InputStream is=urlConnection.getInputStream();
-                    InputStreamReader isr=new InputStreamReader(is);
-
-                    int data=isr.read();
-                    while(data!=-1)
-                    {
-                        current+=(char)data;
-                        data=isr.read();
+                    @Override
+                    public void onNext(MainRecyclerRepo mainRecyclerRepo) {
+                         Log.d("bannerData", mainRecyclerRepo.getMoviesList().toString());
+                        List<MovieModelClass> recyclerRepoMoviesList= mainRecyclerRepo.getMoviesList();
+                        recyclerViewAdapter.SetAdapter(recyclerRepoMoviesList);
                     }
-                    return current;
-                }
-                catch (MalformedURLException e)
-                {
-                    e.printStackTrace();
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
-                finally {
-                    if(urlConnection!=null)
-                    {
-                        urlConnection.disconnect();
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d("bannerData", "" + e);
                     }
-                }
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-            return current;
-        }
-        @Override
-        protected void onPostExecute(String s){
-            List<MovieModelClass> movieList=new ArrayList<>();
-            try{
-                JSONObject jsonObject=new JSONObject(s);
-                JSONArray jsonArray=jsonObject.getJSONArray("results");
 
-                for(int i=0;i<jsonArray.length();i++){
-                    JSONObject jsonObject1=jsonArray.getJSONObject(i);
+                    @Override
+                    public void onComplete() {
 
-                    MovieModelClass model=new MovieModelClass();
-                    model.setId(jsonObject1.getString("name"));
-                    model.setOriginal_title(jsonObject1.getString("original_name"));
-                    model.setPoster_path(jsonObject1.getString("poster_path"));
+                    }
+                })
 
-                    movieList.add(model);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            PutDataIntoRecyclerView(movieList,adapter,recyclerView,linearLayoutManager);
-        }
+        );
     }
-    public static void PutDataIntoRecyclerView(List<MovieModelClass> movieList,Adapter adapter,RecyclerView recyclerView,LinearLayoutManager linearLayoutManager){
-        adapter.SetAdapter(movieList);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(adapter);
-    }
+
+
 }
